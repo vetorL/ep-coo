62d22a9713643ee80a7487b76fb94b01bd1845ea
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-15 14:37:27 -0300

    Create ProjectileManager class

---
+++ src/ProjectileManager.java
@@ -0,0 +1,48 @@
+public class ProjectileManager {
+
+    private State [] states;                // estados
+    private double [] X;				    // coordenadas x
+    private double [] Y;				    // coordenadas y
+    private double [] VX;				    // velocidades no eixo x
+    private double [] VY;				    // velocidades no eixo y
+
+    public State[] getStates() {
+        return states;
+    }
+
+    public void setStates(State[] states) {
+        this.states = states;
+    }
+
+    public double[] getX() {
+        return X;
+    }
+
+    public void setX(double[] x) {
+        X = x;
+    }
+
+    public double[] getY() {
+        return Y;
+    }
+
+    public void setY(double[] y) {
+        Y = y;
+    }
+
+    public double[] getVX() {
+        return VX;
+    }
+
+    public void setVX(double[] VX) {
+        this.VX = VX;
+    }
+
+    public double[] getVY() {
+        return VY;
+    }
+
+    public void setVY(double[] VY) {
+        this.VY = VY;
+    }
+}
