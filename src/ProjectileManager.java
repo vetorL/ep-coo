679320792b06b92f410ff812740ddef1e3c72abf
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-25 23:52:30 -0300

    Add getters and setters to ProjectileManager

--- src/ProjectileManager.java
+++ src/ProjectileManager.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class ProjectileManager {
@@ -28,5 +29,75 @@ public abstract class ProjectileManager {
 
     public List<Projectile> getProjectiles() {
         return projectiles;
+    }
+
+    public State[] getStates() {
+        State [] states = new State[projectiles.size()];
+        for (int i = 0; i < projectiles.size(); i++) {
+            states[i] = projectiles.get(i).getState();
+        }
+        return states;
+    }
+
+    public void setStates(State[] states) {
+        for (int i = 0; i < projectiles.size(); i++) {
+            projectiles.get(i).setState(states[i]);
+        }
+    }
+
+    public double[] getY() {
+        double [] y = new double[projectiles.size()];
+        for (int i = 0; i < projectiles.size(); i++) {
+            y[i] = projectiles.get(i).getY();
+        }
+        return y;
+    }
+
+    public void setY(double[] y) {
+        for (int i = 0; i < projectiles.size(); i++) {
+            projectiles.get(i).setY(y[i]);
+        }
+    }
+
+    public double[] getX() {
+        double [] x = new double[projectiles.size()];
+        for (int i = 0; i < projectiles.size(); i++) {
+            x[i] = projectiles.get(i).getX();
+        }
+        return x;
+    }
+
+    public void setX(double[] x) {
+        for (int i = 0; i < projectiles.size(); i++) {
+            projectiles.get(i).setX(x[i]);
+        }
+    }
+
+    public double[] getVX() {
+        double [] vx = new double[projectiles.size()];
+        for (int i = 0; i < projectiles.size(); i++) {
+            vx[i] = projectiles.get(i).getVX();
+        }
+        return vx;
+    }
+
+    public void setVX(double[] vx) {
+        for (int i = 0; i < projectiles.size(); i++) {
+            projectiles.get(i).setVX(vx[i]);
+        }
+    }
+
+    public double[] getVY() {
+        double [] vy = new double[projectiles.size()];
+        for (int i = 0; i < projectiles.size(); i++) {
+            vy[i] = projectiles.get(i).getVY();
+        }
+        return vy;
+    }
+
+    public void setVY(double[] vy) {
+        for (int i = 0; i < projectiles.size(); i++) {
+            projectiles.get(i).setVY(vy[i]);
+        }
     }
 }
