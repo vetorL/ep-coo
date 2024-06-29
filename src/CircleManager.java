75640dac1d882de07a368654901ce12cd0b00c7a
vetorL <145482274+vetorL@users.noreply.github.com>
2024-06-29 15:33:04 -0300

    Refatora updatePosition e tryLaunch

--- src/CircleManager.java
+++ src/CircleManager.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 
 public class CircleManager extends EnemyManager {
 
-    private long [] enemy1_nextShoot = new long[10];
-
     public CircleManager(long nextEnemy) {
         super(nextEnemy);
 
@@ -18,41 +16,19 @@ public class CircleManager extends EnemyManager {
 
     public void tryLaunch(long currentTime) {
         if(currentTime > getNextEnemy()){
+            Circle circle = new Circle();
 
-            int free = Main.findFreeIndex(getStates());
+            circle.setX(Math.random() * (GameLib.WIDTH - 20.0) + 10.0);
+            circle.setY(-10.0);
+            circle.setV(0.20 + Math.random() * 0.15);
+            circle.setAngle(3 * Math.PI / 2);
+            circle.setRV(0.0);
+            circle.setState(State.ACTIVE);
+            circle.setNextShot(currentTime + 500);
 
-            if(free < getStates().length){
+            setNextEnemy(currentTime + 500);
 
-                double [] enemy1_X = getX();
-                enemy1_X[free] = Math.random() * (GameLib.WIDTH - 20.0) + 10.0;
-                setX(enemy1_X);
-
-                double [] enemy1_Y = getY();
-                enemy1_Y[free] = -10.0;
-                setY(enemy1_Y);
-
-                double [] enemy1_V = getV();
-                enemy1_V[free] = 0.20 + Math.random() * 0.15;
-                setV(enemy1_V);
-
-                double [] enemy1_angle = getAngle();
-                enemy1_angle[free] = 3 * Math.PI / 2;
-                setAngle(enemy1_angle);
-
-                double [] enemy1_RV = getRV();
-                enemy1_RV[free] = 0.0;
-                setRV(enemy1_RV);
-
-                State [] enemy1_states = getStates();
-                enemy1_states[free] = State.ACTIVE;
-                setStates(enemy1_states);
-
-                this.enemy1_nextShoot[free] = currentTime + 500;
-
-                long nextEnemy1 = currentTime + 500;
-                setNextEnemy(nextEnemy1);
-
-            }
+            getEnemies().add(circle);
         }
     }
 
@@ -62,7 +38,7 @@ public class CircleManager extends EnemyManager {
                                Player player) {
         for(int i = 0; i < getStates().length; i++){
 
-            Enemy circle = getEnemies().get(i);
+            Circle circle = (Circle) getEnemies().get(i);
 
             if(circle.getState() == State.EXPLODING){
                 if(currentTime > circle.getExplosion_end()){
@@ -82,7 +58,7 @@ public class CircleManager extends EnemyManager {
                     circle.setY(circle.getY() + (circle.getV() * Math.sin(circle.getAngle()) * delta * (-1.0)));
                     circle.setAngle(circle.getAngle() + (circle.getRV() * delta));
 
-                    if(currentTime > enemy1_nextShoot[i] && circle.getY() < player.getY()){
+                    if(currentTime > circle.getNextShot() && circle.getY() < player.getY()){
                         Projectile projectile = new EnemyProjectile(enemyProjectileManager.getRadius());
 
                         projectile.setX(circle.getX());
@@ -95,7 +71,8 @@ public class CircleManager extends EnemyManager {
 
                         projectile.setState(State.ACTIVE);
 
-                        enemy1_nextShoot[i] = (long) (currentTime + 200 + Math.random() * 500);
+                        long nextShot = (long) (currentTime + 200 + Math.random() * 500);
+                        circle.setNextShot(nextShot);
 
                         enemyProjectileManager.add(projectile);
                     }
